b9bfa4f0e3f5680535b741c91566bed9f189dc3a
spike <86832478+spikeflcl@users.noreply.github.com>
2022-01-15 18:08:02 +0100

    Backend products (#7)
    * add adding products

---
+++ backend/routes/products.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/product');
+const User = require('../models/user');
+
+router.post('/add', (req, res) => {
+  const ownerId = req.body.userId;
+  const images = req.body.images;
+  const description = req.body.description;
+  const productName = req.body.productName;
+  const location = req.body.location;
+  const token = req.body.token;
+
+  if (!ownerId || !images || !description || !productName || !location || !token) return res.status(400).send({msg: 'Missing data. Check input.'});
+
+  User.findOne({_id: ownerId}, (err, obj) => {
+    if (err) console.log(err);
+
+    if (token !== obj.sessionToken) return res.status(401).send({msg: 'Wrong token.'}); 
+  });
+
+
+
+  const product = new Product({ownerId, images, description, productName, location});
+  product.save(function(err, product) {
+    if (err) return res.status(401).send({msg: "Couldn't save product. Check input data."});
+    res.status(201).send({msg: 'Product added!', productId: product._id});
+  })
+});
+
+module.exports = router;
