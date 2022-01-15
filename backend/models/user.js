1679cfbdf6df4b7ac62152a4d026225326169452
Spike <lukasz.piwowar1@protonmail.com>
2022-01-15 20:52:20 +0100

    add adding, editing and deleting reviews

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,12 +4,11 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   name: {type: String, required: true, minLength: 3, maxLength: 100},
   surname: {type: String, required: true, minLength: 3, maxLength: 100},
-  username: {type: String, required: true, minLength: 3, maxLength: 100, unique: true},
+  username: {type: String, required: true, minLength: 5, maxLength: 100, unique: true},
   password: {type: String, required: true, minLength: 8, maxLength: 150},
   created: {type: Date, default: Date.now()},
   email: {type: String, required: true, unique: true},
-  sessionToken: {type: String},
-  products: [Schema.Types.ObjectId]
+  sessionToken: {type: String}
 });
 
 userSchema
