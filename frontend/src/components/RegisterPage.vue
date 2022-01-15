d176458fad1e76b97b0b05281417ebdcdbdef8c8
Dodonito <dominik14081999@wp.pl>
2022-01-15 18:15:40 +0100

    feat: validation first steps

--- frontend/src/components/RegisterPage.vue
+++ frontend/src/components/RegisterPage.vue
@@ -1,19 +1,37 @@
 <template>
-  <div :class="$style['wrapper']" @keyup.enter.stop="addUser">
-    <CvForm :class="$style['register-box']">
+  <div :class="$style['wrapper']" @keyup.enter.stop="register">
+    <CvForm :class="$style['register-box']" @submit="checkForm">
       <h2>Register</h2>
-      <CvTextInput type="email" v-model="userEmail" placeholder="Email"></CvTextInput>
-      <CvTextInput v-model="username" placeholder="User Name"></CvTextInput>
-      <CvTextInput v-model="name" placeholder="Name"></CvTextInput>
-      <CvTextInput v-model="surname" placeholder="Surname"></CvTextInput>
-      <CvTextInput v-model="password" placeholder="Password"></CvTextInput>
-      <CvButton @click="addUser">Create Account</CvButton>
+      <p>Email</p>
+      <CvTextInput type="email" v-model="userEmail" placeholder="Email" min-length="3" required invalid-message="true"></CvTextInput>
+      <p>Username</p>
+      <CvTextInput v-model="username" placeholder="User Name" min-length="3" required></CvTextInput>
+      <p>First Name</p>
+      <CvTextInput v-model="name" placeholder="Name" min-length="3" required></CvTextInput>
+      <p>Surname</p>
+      <CvTextInput v-model="surname" placeholder="Surname" min-length="3" required></CvTextInput>
+      <p>Password</p>
+      <CvTextInput v-model="password" placeholder="Password" min-length="3" required></CvTextInput>
+      <CvButton @click="register">Create Account</CvButton>
     </CvForm>
+    <CvToastNotification
+      :class="[
+        $style['notification'],
+        {
+          [$style['notification--opened']]: errorVisible,
+          [$style['notification--closed']]: !errorVisible
+        },
+      ]"
+      kind="error"
+      title="Error"
+      :caption="errorMessage"
+      @close="errorVisible = false"
+    />
   </div>
 </template>
 
 <script>
-import { CvButton, CvTextInput, CvForm } from '@carbon/vue'
+import { CvButton, CvTextInput, CvForm, CvToastNotification } from '@carbon/vue'
 import axios from 'axios';
 
 export default {
@@ -21,7 +39,8 @@ export default {
   components: {
     CvButton,
     CvTextInput,
-    CvForm
+    CvForm,
+    CvToastNotification
   },
   data() {
     return {
@@ -29,20 +48,29 @@ export default {
       username: '',
       password: '',
       name: '',
-      surname: ''
+      surname: '',
+      validation_error: [],
+      errorVisible: true,
+      errorMessage: ''
     }
   },
   methods: {
-    async addUser() {
-      await axios.post('users/register', {username: this.username, password: this.password});
-      this.$router.push('/login');
+    async register() {
+      await axios.post('users/register', {username: this.username, password: this.password})
+        .catch((error) => {
+          this.validation_error.push(error);
+          this.errorVisible = true;
+        })
+      if (!this.validation_error) {
+        this.$router.push('/');
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" module>
-@import '@/assets/_variables';
+@import '@carbon/themes/scss/themes';
 
 .wrapper {
   display: flex;
@@ -50,13 +78,27 @@ export default {
   align-items: center;
   width: 100vw;
   height: 100vh;
-  color: $primary;
-  background-color: $background;
+  // color: $primary;
+  // background-color: $background;
 }
 
 .register-box {
   width: 50%;
   height: 50%;
-  background-color: $secondary;
+  // background-color: $secondary;
+}
+
+.notification {
+  position: absolute;
+  top: 5%;
+  transition: left 0.3s ease-in-out;
+
+  &--opened {
+    left: 5%;
+  }
+
+  &--closed {
+    left: -35%;
+  }
 }
 </style>
