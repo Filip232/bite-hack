38b4c518edc16a56ebc981bbc7fe96bfeabf5999
MaciejRadym <48200554+MaciejRadym@users.noreply.github.com>
2022-01-15 14:31:19 +0100

    Feat  main page (#3)
    * feat: styles, font
    
    * feat: font fallback
    
    * feat: element ui
    
    * feat: element ui
    
    * feat: carbon ui
    
    * fix: element ui
    
    * feat: env

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios';
+import dotenv from 'dotenv'
 
-axios.defaults.baseURL = 'http://192.168.43.130:3000/';
+dotenv.config()
+console.log(process.env.AXIOS_URL)
+
+axios.defaults.baseURL = process.env.AXIOS_URL;
 axios.defaults.headers.post['crossDomain'] = 'true';
+import "carbon-components/css/carbon-components.min.css";
+import CarbonComponentsVue from "@carbon/vue";
+
+Vue.use(CarbonComponentsVue);
 
 Vue.config.productionTip = false
 
