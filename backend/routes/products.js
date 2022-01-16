2497de6442e23c4d323f5b7326fc2676da5a8d15
Spike <lukasz.piwowar1@protonmail.com>
2022-01-16 01:30:32 +0100

    adding paginated product list by category

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,16 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/product');
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 router.post('/add', (req, res) => {
-  const ownerId = req.body.userId;
+  const ownerId = req.body.ownerId;
   const images = req.body.images;
   const description = req.body.description;
   const productName = req.body.productName;
   const location = req.body.location;
   const token = req.body.token;
+  const category = req.body.category;
 
-  if (!ownerId || !images || !description || !productName || !location || !token) return res.status(400).send({msg: 'Missing data. Check input.'});
+  if (!ownerId || !images || !description || !productName || !location || !token || !category) return res.status(400).send({msg: 'Missing data. Check input.'});
 
   User.findOne({_id: ownerId}, (err, obj) => {
     if (err) return console.log(err);
@@ -20,14 +22,34 @@ router.post('/add', (req, res) => {
       if (err) return console.log(err);
 
       if (!result) res.status(401).send({msg: 'Wrong session token.'});
+
+      const product = new Product({ownerId, images, description, productName, location, category});
+      product.save( (err, product) => {
+        if (err) return res.status(401).send({msg: "Couldn't save product. Check input data.", err: err});
+        res.status(201).send({msg: 'Product added!', productId: product._id});
+      });
     }); 
   });
-
-  const product = new Product({ownerId, images, description, productName, location});
-  product.save( (err, product) => {
-    if (err) return res.status(401).send({msg: "Couldn't save product. Check input data."});
-    res.status(201).send({msg: 'Product added!', productId: product._id});
-  })
 });
+
+router.get("/productList/:page", (req, res) => {
+  const page = parseInt(req.params.page) || 0; //for next page pass 1 here
+  const limit = parseInt(req.query.limit) || 50;
+  const category = req.query.category;
+  console.log(page, limit, category);
+  Product.find({category: category})
+    .sort({ update_at: -1 })
+    .skip(page * limit) //Notice here
+    .limit(limit)
+    .exec((err, doc) => {
+      if (err) return console.log(err);
+
+      Product.countDocuments({category}).exec((err, count) => {
+        if (err) return console.log(err);
+        return res.status(200).send({total: count, page: page, pageSize: doc.length, products: doc, maxPage: Math.ceil(count/limit)});
+      });
+    });
+});
+
 
 module.exports = router;
